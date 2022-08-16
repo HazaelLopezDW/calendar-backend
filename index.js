602f62afcea929854566a5de80685cdd2e32a00b
Hazael Lopez <elhazael134@gmail.com>
2022-08-16 17:01:33 -0600

    Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo...');
+const express = require('express')
+const app = express()
+
+app.get('/',  (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Servidor Corriendo en port ${process.env.PORT}`);
+});
