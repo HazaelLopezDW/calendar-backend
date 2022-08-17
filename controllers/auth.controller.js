a7ea1eaf82f155ec2e3ff4121c2310aa4dda05d8
Hazael Lopez <elhazael134@gmail.com>
2022-08-17 17:31:24 -0600

    Login de usuario

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,10 +6,8 @@ const crearUsuario = async (req, res = response) => {
 
     const { email, password } = req.body;
 
-    // Verificar si el emial ya esta registrado 
-
     try {
-
+        // Verificar si el emial ya esta registrado 
         let usuario = await Usuario.findOne({ email });
         
         if(usuario){
@@ -41,16 +39,47 @@ const crearUsuario = async (req, res = response) => {
     }
 }
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
 
     const {email, password} = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: `Login usuario`,
-        email, 
-        password
-    });
+    try {
+
+        // Verificar si el emial ya esta registrado 
+        const usuario = await Usuario.findOne({ email });
+        
+        if(!usuario){
+            return res.status(400).json({
+                ok:false,
+                msg: `El usuario no existe con ese email`
+            })
+        }
+
+        // Confirmar los password
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password Incorrecto'
+            })
+        }
+
+        // TODO: Generar JWt
+
+        res.status(200).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: `Por favor hable con el administrador`
+        })
+    }
 }
 
 const revalidarToken = (req, res = response) => {
